8bb7f72d6ada0a1a99e3e423d253ac5a5764fe4b
猪野麻巳子 <p@iinomamiko-no-MacBook-Pro.local>
2018-07-01 14:53:23 +0900

    only one train ver

--- python/trainGuide/stephw6-2.py
+++ python/trainGuide/stephw6-2.py
@@ -1,11 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import webapp2
 import os
 from google.appengine.api import urlfetch
 import json, codecs
 import jinja2
+import cgi
 
-f=codecs.open("trainData.json","r","utf-8")
+fromToData=[]
+f=codecs.open("aliceData.json","r","utf-8")
 trainData=json.load(f)
+
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -13,7 +19,7 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 
 class BaseHandler(webapp2.RequestHandler):
-    def render(self, html, values={"graph": trainData}):
+    def render(self, html, values={"graph": trainData, "graph2":fromToData}):
         template = JINJA_ENVIRONMENT.get_template(html)
         self.response.write(template.render(values))
 
@@ -24,7 +30,38 @@ class MainHandler(BaseHandler):
 class SabHandler(BaseHandler):
     def get(self):
         self.render("stephw6-2Page2.html")
-    
+        fromSta=cgi.escape(self.request.get("from"))
+        toSta=cgi.escape(self.request.get("to"))
+        trainLine=["Mimsy Line","Burrow Grove Express","Chesire","Queens Line", "Caterpillar Way"]
+        fromLine=[]
+        toLine=[]
+        length=0
+        for dict in trainData:
+            length+=1
+            if fromSta in dict.get("Stations"):
+                fromLine.append(1)
+            else:
+                fromLine.append(0) 
+
+        for dict in trainData:    
+            if toSta in dict.get("Stations"):
+                toLine.append(1)
+            else:
+                toLine.append(0)
+
+        print(fromLine)
+        print(toLine)
+        i=0
+        while i<length:
+            if fromLine[i] == toLine[i] and fromLine[i]==1:
+                fromToData.append({"print":fromSta})
+                fromToData.append({"print":trainLine[i]})
+                fromToData.append({"print":toSta})
+                fromToData.append({"print":"--------------------------------------"})
+                print(fromToData)
+            else:
+                pass
+            i+=1
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
