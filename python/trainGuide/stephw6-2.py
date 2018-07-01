bacbf0454f53757707d694aad1e3a749765ff7a6
猪野麻巳子 <p@iinomamiko-no-MacBook-Pro.local>
2018-07-01 09:39:34 +0900

    updated

--- python/trainGuide/stephw6-2.py
+++ python/trainGuide/stephw6-2.py
@@ -1,40 +1,19 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 import webapp2
-import json,codecs
 import os
+from google.appengine.api import urlfetch
+import json, codecs
 import jinja2
-import cgi
-#from bs4 import BeautifulSoup
 
-#with open("traindata2.xml") as traindata:
-    #soup =BeautifulSoup(traindata, "xml")
-#print(soup)
-
-# read json file
 f=codecs.open("trainData.json","r","utf-8")
-json_dict=json.load(f)
-print("json_dict:{}".format(type(json_dict)))
-print(json_dict)
-#json dict -> json String
-print("json dict -> json String")
-json_str = json.dumps(json_dict)
-print("json_str:{}".format(type(json_str)))
-print(json_str)
-print("json String -> json dict")
-json_dict2=json.loads(json_str)
-print("json_dict2:{}".format(type(json_dict2)))
-print(json_dict2)
+trainData=json.load(f)
 
-        
-    
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
 class BaseHandler(webapp2.RequestHandler):
-    def render(self, html, values={}):
+    def render(self, html, values={"graph": trainData}):
         template = JINJA_ENVIRONMENT.get_template(html)
         self.response.write(template.render(values))
 
